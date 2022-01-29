3af9d10a747844171269168bf0508340891673c1
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-01-29 11:58:18 +0900

    display web3 version

--- frontend/app/src/App.vue
+++ frontend/app/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <Contract />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Contract from './components/Contract.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Contract
   }
 }
 </script>
---
+++ frontend/app/src/components/Contract.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ version }}
+  </div>
+</template>
+
+<script>
+import web3 from "web3";
+export default {
+  setup() {
+    const version = web3.version;
+    return {
+      version,
+    };
+  },
+};
+</script>
