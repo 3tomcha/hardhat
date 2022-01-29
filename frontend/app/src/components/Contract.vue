845e7fccd9b056b257fea801e6adf96862f14a5f
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-01-29 12:49:24 +0900

    attach contract

--- frontend/app/src/components/Contract.vue
+++ frontend/app/src/components/Contract.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
-    {{ version }}
   </div>
 </template>
 
-<script>
-import web3 from "web3";
+<script lang="ts">
+import Web3 from 'web3'
+import helloworld from '../assets/HelloWorld.json'
 export default {
-  setup() {
-    const version = web3.version;
+  async setup () {
+    const web3 = await new Web3('https://ropsten.infura.io/v3/afbbf905acb14dad8bd872c628058f52')
+    const contractAddress = '0xF00125Fa190be6f186e50aA44bC35bb8F508Dd6e'
+    // const helloworld = require('../asserts/HelloWorld.json')
+    // console.log(JSON.stringify(decycle(helloworld)))
+    const contract = await new web3.eth.Contract(helloworld.abi, contractAddress)
+    console.log(contract)
     return {
-      version,
-    };
-  },
-};
+      contract
+    }
+  }
+}
 </script>
