d6ca123d43d7f70ac4039a192f1af50f0312ee03
Tomoya Kobayashi <tomocha.marika@gmail.com>
2022-01-30 10:33:18 +0900

    call contract method

--- frontend/app/src/components/Contract.vue
+++ frontend/app/src/components/Contract.vue
@@ -3,17 +3,17 @@
   </div>
 </template>
 
-<script lang="ts">
+<script>
 import Web3 from 'web3'
 import helloworld from '../assets/HelloWorld.json'
 export default {
   async setup () {
     const web3 = await new Web3('https://ropsten.infura.io/v3/afbbf905acb14dad8bd872c628058f52')
     const contractAddress = '0xF00125Fa190be6f186e50aA44bC35bb8F508Dd6e'
-    // const helloworld = require('../asserts/HelloWorld.json')
-    // console.log(JSON.stringify(decycle(helloworld)))
-    const contract = await new web3.eth.Contract(helloworld.abi, contractAddress)
-    console.log(contract)
+    const contract = await new web3.eth.Contract(helloworld, contractAddress)
+    const helllo = await contract.methods.hello().call()
+    console.log(helllo)
+    alert(helllo)
     return {
       contract
     }
